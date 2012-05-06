e6d8444a665867e191cdb50d7af642bd55a059c0
Allison Levine <allisonlevine@gmail.com>
2012-05-06 12:31:59 -0400

    Add files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spot the Difference Game with JQuery
+
+*DESCRIPTION*
+Users identify the differences between two photos by clicking on them.  The spots are highlighted when clicked.  When all the spots have been clicked, an email form appears so that eligible users can enter a "photo hunt" contest.
+
+*HOW IT WORKS*
+A transparent, clickable overlay is absolutely positioned over the "different" image on the right.  It's an image map with a different area for each difference.  The JQuery highlights each area as it's clicked, and then shows the email form when all the areas have been clicked.
---
+++ sendEmail.php
@@ -0,0 +1,35 @@
+﻿<?php
+	
+	$name = trim($_POST['name']);
+	$email = $_POST['email'];
+	$comments = $_POST['comments'];
+	
+
+	$site_owners_email = 'test@test.com'; // Replace this with your own email address
+	$site_owners_name = 'Test Name'; // replace with your name
+	
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "Please enter a valid email address.";	
+	}
+	
+	if (!$error) {
+		
+		$to = $site_owners_email;
+		$subject = "Spot the Difference Contest Entry";
+		$body = "Email: $email\n";
+		$headers .= 'From: ' . $email . "\r\n";
+		mail($to, $subject, $body, $headers);
+		
+		echo "<li class='success'> Success!  Your email address was entered to win. </li>";
+		
+	} # end if no error
+	else {
+
+		$response = (isset($error['name'])) ? "<li>" . $error['name'] . "</li> \n" : null;
+		$response .= (isset($error['email'])) ? "<li>" . $error['email'] . "</li> \n" : null;
+		$response .= (isset($error['comments'])) ? "<li>" . $error['comments'] . "</li>" : null;
+		
+		echo $response;
+	} # end if there was an error sending
+
+?>
